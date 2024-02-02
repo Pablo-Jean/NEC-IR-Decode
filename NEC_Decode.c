9e3ad8c0a6320b08f672ea4e9fd0dbfc665cfcbd
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2024-02-01 21:17:41 -0300

    :minidisc: implemented a debug, init, and reduced chipset dependency

--- NEC_Decode.c
+++ NEC_Decode.c
@@ -7,25 +7,32 @@
 
 #include "NEC_Decode.h"
 
+#if defined (NEC_DEBUG)
+// TODO The printf :)
+#define NEC_DEBUG(...)			printf(__VA_ARGS__)
+#else
+#define NEC_DEBUG(...)
+#endif
+
 void NEC_TIM_IC_CaptureCallback(NEC* handle) {
 
     if (handle->state == NEC_INIT) {
 
-        HAL_TIM_IC_Stop_DMA(handle->timerHandle, handle->timerChannel);
-
+    	handle->NEC_InCapt_Stop();
         if (handle->rawTimerData[1] < handle->timingAgcBoundary) {
+			NEC_DEBUG("Received Repeat command\n");
             handle->state = NEC_OK;
             handle->NEC_RepeatCallback();
         } else {
+        	NEC_DEBUG("Received AGC command\n");
             handle->state = NEC_AGC_OK;
-            HAL_TIM_IC_Start_DMA(handle->timerHandle, handle->timerChannel,
-                    (uint32_t*) handle->rawTimerData, 32);
+            handle->NEC_InCapt_Start((uint32_t*) handle->rawTimerData, 32);
         }
 
     } else if (handle->state == NEC_AGC_OK) {
 
-        HAL_TIM_IC_Stop_DMA(handle->timerHandle, handle->timerChannel);
-
+    	handle->NEC_InCapt_Stop();
+    	NEC_DEBUG("Received the Data\n");
         for (int pos = 0; pos < 32; pos++) {
             int time = handle->rawTimerData[pos];
             if (time > handle->timingBitBoundary) {
@@ -40,22 +47,56 @@ void NEC_TIM_IC_CaptureCallback(NEC* handle) {
         uint8_t naddr = ~handle->decoded[0];
         uint8_t ncmd = ~handle->decoded[2];
 
+        // check if the Address is match with the complementaryn Address
+        // only if is NOT Extended
         if (handle->type == NEC_NOT_EXTENDED && handle->decoded[1] != naddr)
             valid = 0;
+        // Check if Command matches the complementary
         if (handle->decoded[3] != ncmd)
             valid = 0;
 
         handle->state = NEC_OK;
 
-        if (valid)
-            handle->NEC_DecodedCallback(handle->decoded[0], handle->decoded[2]);
-        else
+        if (valid){
+        	handle->Command = handle->decoded[2];
+        	if (handle->type == NEC_NOT_EXTENDED)
+        		handle->Address = handle->decoded[0];
+        	else
+        		handle->Address = handle->decoded[0] | (handle->decoded[1] << 8);
+            NEC_DEBUG("NEC packet is valid! We received the following data:\n");
+            NEC_DEBUG("> Address: 0x%04X\n"
+            		  "> Command: %d\n",
+            		handle->Address, handle->Command);
+        	handle->NEC_DecodedCallback(handle->Address, handle->Command);
+        }
+		else
+			NEC_DEBUG("The received packet is corrupted or incorrectly configured\n");
             handle->NEC_ErrorCallback();
     }
 }
 
+NEC_STATE NEC_Init(NEC* handle){
+	if (handle->NEC_DecodedCallback == NULL ||
+		handle->NEC_ErrorCallback == NULL ||
+		handle->NEC_InCapt_Start == NULL ||
+		handle->NEC_InCapt_Stop == NULL ||
+		handle->NEC_RepeatCallback == NULL){
+
+		NEC_DEBUG("Failed to Start, missing functions for &08X handle", (uint32_t)handle);
+		return NEC_FAIL;
+		}
+	memset(handle->rawTimerData, 0, sizeof(handle->rawTimerData));
+	NEC_DEBUG("NEC decoded start for %08X Handle", (uint32_t)handle);
+
+	return NEC_OK;
+}
+
+void NEC_DeInit(NEC* handle){
+	handle->NEC_InCapt_Stop();
+}
+
 void NEC_Read(NEC* handle) {
     handle->state = NEC_INIT;
-    HAL_TIM_IC_Start_DMA(handle->timerHandle, handle->timerChannel,
-            (uint32_t*) handle->rawTimerData, 2);
+    handle->NEC_InCapt_Start((uint32_t*) handle->rawTimerData, 2);
+    NEC_DEBUG("Started NEC Reading\n");
 }
--- NEC_Decode.h
+++ NEC_Decode.h
@@ -9,42 +9,54 @@
 #define INC_NEC_DECODE_H_
 
 #include <stdint.h>
-#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
+
+
 
 typedef enum {
     NEC_NOT_EXTENDED, NEC_EXTENDED
 } NEC_TYPE;
 
 typedef enum {
-    NEC_INIT, NEC_AGC_OK, NEC_AGC_FAIL, NEC_FAIL, NEC_OK
+	NEC_OK, NEC_INIT, NEC_AGC_OK, NEC_AGC_FAIL, NEC_FAIL
 } NEC_STATE;
 
 typedef struct {
-    int rawTimerData[32];
+    uint32_t rawTimerData[32];
     uint8_t decoded[4];
+    uint16_t Address;
+    uint8_t Command;
 
     NEC_STATE state;
 
-    TIM_HandleTypeDef *timerHandle;
-
-    uint32_t timerChannel;
-    HAL_TIM_ActiveChannel timerChannelActive;
-
+    // The timer of value to indicate if the value is 1 or 0
+    // if the value of Input Capture is > timingBitBoundary = '1'
+    // else if Input Capture is < timingBitBoundary = '0'
     uint16_t timingBitBoundary;
+    // The timing of the Pulse of Agc, if the value is less
+    // than the timingAgcBoundary, we have a Repeat Code
     uint16_t timingAgcBoundary;
     NEC_TYPE type;
 
-    void (*NEC_DecodedCallback)(uint16_t, uint8_t);
+    void (*NEC_InCapt_Start)(uint32_t *Arr, uint8_t len);
+    void (*NEC_InCapt_Stop)();
+
+    void (*NEC_DecodedCallback)(uint16_t Addr, uint8_t Cmd);
     void (*NEC_ErrorCallback)();
     void (*NEC_RepeatCallback)();
 } NEC;
 
-void NEC_Init(NEC* handle);
+// Init NEC
+NEC_STATE NEC_Init(NEC* handle);
 
+// deinit de NEC
 void NEC_DeInit(NEC* handle);
 
+// Callback of the timer
 void NEC_TIM_IC_CaptureCallback(NEC* handle);
 
+// Start Read one time. Need to be called every Callback
 void NEC_Read(NEC* handle);
 
 #endif /* INC_NEC_DECODE_H_ */
